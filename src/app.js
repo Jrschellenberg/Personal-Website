a0584c5488cf291fd8ddefaa587f703a92749ddd
Justin Schellenberg <jrschellenberg@gmail.com>
2018-04-10 16:26:13 -0500

    Continue Scaffolding out project from previous project

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const config = require('config');
+const dbConfig = config.get('DBHost');
+const expressSanitizer = require('express-sanitizer');
 
-var index = require('./routes/index');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,30 +18,47 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+	app.use(logger('dev'));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressSanitizer());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Middleware to set variable for view if our project is in production environment or not
+app.use((req, res, next) => {
+	if (config.util.getEnv('NODE_ENV') === 'production') {
+		res.locals.production = true;
+	}
+	next();
+});
+
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+	let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	
+	// render the error page
+	res.status(err.status || 500);
+	if(err.link){
+		res.redirect(err.link+'?errorMessage='+res.locals.message+'&errorStatus='+err.status+'&error='+res.locals.error);
+	}
+	else{ //Fall back if can't redirect error to same page.
+		res.render('error', { title: "Error", errorMessage: res.locals.message, errorStatus: err.status, error: res.locals.error });
+	}
 });
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+import Utils from './utils';
+import {loggedOut, getUser, passCaptcha} from '../middleware/index';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/',  (req, res) => {
+	return Utils.renderView('page/index', { title: 'Home', navId: '#homeLink', recaptcha: true }, req, res)
 });
 
 module.exports = router;
