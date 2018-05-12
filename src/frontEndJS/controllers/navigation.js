c6f99633addac3c5ee31fbaf59ce455d00da1ebf
Justin Schellenberg <jrschellenberg@gmail.com>
2018-05-12 01:57:46 -0500

    Built middleware for nav buttons in vanilla javascript, couldn't find useful library :sleepy:

--- src/frontEndJS/controllers/navigation.js
+++ src/frontEndJS/controllers/navigation.js
@@ -1,17 +1,48 @@
 export default class NavigationController {
 	constructor(){
-		this.process();
+		this.oldTotalHeight = null
+		this.newTotalHeight = null;
+		this.scrollSpy()
 	}
 	
-	process(){
-		/*
-		This functionality is to make dropdown show on hover rather than onClick.
-		 */
-		$('body').on('mouseenter mouseleave','.dropdown',function(e){
-			var _d=$(e.target).closest('.dropdown');_d.addClass('show');
-			setTimeout(function(){
-				_d[_d.is(':hover')?'addClass':'removeClass']('show');
-			},300);
+	scrollSpy(){
+		let controller = this;
+		controller.setHTML('#Top', '#About');
+		let top = document.getElementById('Top');
+		let about = document.getElementById('About');
+		let timeline = document.getElementById('Timeline');
+		let projects = document.getElementById('Projects');
+		let treehouse = document.getElementById('Treehouse');
+		let contact = document.getElementById('Contact');
+		$(window).on('scroll', (e) => {
+			controller.newTotalHeight = top.offsetHeight; //Initialize our instance variables
+			controller.oldTotalHeight = 0;
+			
+			controller.testNewHeight('#Top', '#About', top.offsetHeight, about.offsetHeight );
+			controller.testNewHeight('#Top', '#Timeline', about.offsetHeight, timeline.offsetHeight );
+			controller.testNewHeight('#About', '#Projects', timeline.offsetHeight, projects.offsetHeight );
+			controller.testNewHeight('#Timeline', '#Treehouse', projects.offsetHeight, treehouse.offsetHeight );
+			console.log(`OldTotalHeight: ${controller.oldTotalHeight} \n newTotalHeight: ${controller.newTotalHeight}\n windowScroll: ${window.scrollY}`);
+			controller.testNewHeight('#Projects', '#Contact', treehouse.offsetHeight, contact.offsetHeight );
+			
+			if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) { //Special case when at bottom of document...
+				controller.setHTML('#Treehouse', '#Contact');
+			}
+			
 		});
+	}
+	testNewHeight(prevValue, nextValue, oldTotalHeightAdder, newTotalHeightAddition){
+		let controller = this;
+		if(window.scrollY + 10 >= controller.oldTotalHeight && window.scrollY + 10 < controller.newTotalHeight ){
+			controller.setHTML(prevValue, nextValue);
+		}
+		controller.oldTotalHeight += oldTotalHeightAdder;
+		controller.newTotalHeight += newTotalHeightAddition;
+	}
+	
+	setHTML(prevScroll, nextScroll){
+		let html = '<a data-scroll href="'+prevScroll+'"><span class="prev"></span></a>'+
+			'<a data-scroll href="'+nextScroll+'"><span class="next"></span></a>';
+		document.getElementById('scrollButtons').innerHTML = html;
 	}
 }
